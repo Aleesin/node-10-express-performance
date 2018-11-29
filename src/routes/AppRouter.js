cb6614e6e3b8846e254b9b822ece5b83b6c37fbe
MattMorgis <matt@rappiddevelopment.com>
2018-11-29 10:40:12 -0500

    feat: proxy endpoints

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -4,21 +4,17 @@ const fs = require("fs");
 const util = require("util");
 const pump = require("pump");
 
-const parseSpeakers = speaker =>
-  JSONStream.parse("results.speaker_labels.segments.*", item => {
-    return item.speaker_label === speaker ? item : null;
-  });
-
 const readFilePromise = util.promisify(fs.readFile);
 
 const largeFile = __dirname + "/../../data/phila_parking_violations_2017.json";
-const smallData = __dirname + "../../data/0002-Art-Of-The-Take.json";
+const smallFile = __dirname + "/../../data/0002-Art-Of-The-Take.json";
 
 class AppRouter {
   constructor() {
     this.router = Router();
     this.base();
     this.data();
+    this.proxy();
   }
 
   base() {
@@ -36,15 +32,13 @@ class AppRouter {
       (request, response, next) => {
         const zipcode = request.params.zipcode;
         const readStream = fs.createReadStream(largeFile);
-        // const parse = JSONStream.parse("rows.*", item => {
-        //   return item.zip_code === zipcode ? item : null;
-        // });
-        pump(readStream, response, error => {
+        const parseJson = JSONStream.parse("rows.*", item => {
+          return item.zip_code === zipcode ? item : null;
+        });
+        pump(readStream, parseJson, JSONStream.stringify(), response, error => {
           if (error) {
             if (error.code === "ENOENT") {
-              console.error("Large data file does not exist");
-              console.error("Try running `node bin/download-large-data`");
-              process.exit(-2);
+              this.handleLargeFileError();
             }
             next(error);
           }
@@ -57,17 +51,14 @@ class AppRouter {
       async (request, response, next) => {
         const zipcode = request.params.zipcode;
         try {
-          const jsonData = await readFilePromise(largeFile);
-          const json = JSON.parse(jsonData);
+          const json = await this.readFile(largeFile);
           const filteredResults = json.rows.filter(obj => {
             return obj.zip_code === zipcode;
           });
           response.json(filteredResults);
         } catch (error) {
           if (error.code === "ENOENT") {
-            console.error("Large data file does not exist");
-            console.error("Try running `node bin/download-large-data`");
-            process.exit(-2);
+            this.handleLargeFileError();
           }
         }
       }
@@ -75,7 +66,12 @@ class AppRouter {
 
     this.router.get("/data/small/stream/:speaker", (req, response, next) => {
       const speaker = req.params.speaker;
-      const readStream = fs.createReadStream(smallData);
+      const readStream = fs.createReadStream(smallFile);
+      const parseSpeakers = speaker =>
+        JSONStream.parse("results.speaker_labels.segments.*", item => {
+          return item.speaker_label === speaker ? item : null;
+        });
+
       pump(
         readStream,
         parseSpeakers(speaker),
@@ -94,10 +90,7 @@ class AppRouter {
       async (request, response, next) => {
         try {
           const speaker = request.params.speaker;
-          const jsonData = await readFilePromise(smallData, {
-            encoding: "utf-8"
-          });
-          const json = JSON.parse(jsonData);
+          const json = await this.readFile(smallFile);
           const filteredResults = json.results.speaker_labels.segments.filter(
             obj => {
               return obj.speaker_label === speaker;
@@ -105,11 +98,59 @@ class AppRouter {
           );
           response.json(filteredResults);
         } catch (e) {
-          e.status = 503;
           next(e);
         }
       }
     );
+  }
+  proxy() {
+    this.router.get("/proxy/:size/stream/", (request, response, next) => {
+      const dataSet = this.datasetFromURLParam(request.params.size);
+      const readStream = fs.createReadStream(dataSet);
+      pump(readStream, response, error => {
+        if (error) {
+          if (error.code === "ENOENT") {
+            this.handleLargeFileError();
+          }
+          next(error);
+        }
+      });
+    });
+
+    this.router.get(
+      "/proxy/:size/promise/",
+      async (request, response, next) => {
+        try {
+          const dataSet = this.datasetFromURLParam(request.params.size);
+          const json = await this.readFile(dataSet);
+          response.json(json);
+        } catch (error) {
+          if (error.code === "ENOENT") {
+            this.handleLargeFileError();
+          }
+        }
+      }
+    );
+  }
+
+  async readFile(file) {
+    try {
+      const jsonData = await readFilePromise(file);
+      const json = JSON.parse(jsonData);
+      return json;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  datasetFromURLParam(param) {
+    return param === "large" ? largeFile : smallFile;
+  }
+
+  handleLargeFileError() {
+    console.error("Large data file does not exist");
+    console.error("Try running `node bin/download-large-data`");
+    process.exit(-2);
   }
 
   format(uptime) {
