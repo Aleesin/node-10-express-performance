120f9d4b903c6118c37f8b949b10944fb19b99b7
MattMorgis <matt@rappiddevelopment.com>
2018-11-29 09:54:26 -0500

    feat: adding download script for large data to remve network as a factor

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,14 +1,8 @@
 const {Router} = require("express");
-const fetch = require("node-fetch");
 const JSONStream = require("JSONStream");
 const fs = require("fs");
 const util = require("util");
 const pump = require("pump");
-
-const largeJsonUrl =
-  "https://s3.amazonaws.com/philadelphia-parking-violations-raw-data/parking_violations_2017.json";
-const smallJsonUrl =
-  "https://s3.amazonaws.com/the-art-of-the-take/0002-Art-Of-The-Take.json";
 
 const parseSpeakers = speaker =>
   JSONStream.parse("results.speaker_labels.segments.*", item => {
@@ -16,6 +10,9 @@ const parseSpeakers = speaker =>
   });
 
 const readFilePromise = util.promisify(fs.readFile);
+
+const largeFile = __dirname + "/../../data/phila_parking_violations_2017.json";
+const smallData = __dirname + "../../data/0002-Art-Of-The-Take.json";
 
 class AppRouter {
   constructor() {
@@ -38,118 +35,68 @@ class AppRouter {
       "/data/large/stream/:zipcode",
       (request, response, next) => {
         const zipcode = request.params.zipcode;
-        fetch(largeJsonUrl)
-          .then(res => {
-            const parse = JSONStream.parse("rows.*", item => {
-              return item.zip_code === zipcode ? item : null;
-            });
-            pump(res.body, parse, JSONStream.stringify(), response, error => {
-              if (error) {
-                return next(error);
-              }
-            });
-          })
-          .catch(e => {
-            e.status = 503;
-            return next(e);
-          });
+        const readStream = fs.createReadStream(largeFile);
+        // const parse = JSONStream.parse("rows.*", item => {
+        //   return item.zip_code === zipcode ? item : null;
+        // });
+        pump(readStream, response, error => {
+          if (error) {
+            if (error.code === "ENOENT") {
+              console.error("Large data file does not exist");
+              console.error("Try running `node bin/download-large-data`");
+              process.exit(-2);
+            }
+            next(error);
+          }
+        });
       }
     );
 
     this.router.get(
       "/data/large/promise/:zipcode",
       async (request, response, next) => {
+        const zipcode = request.params.zipcode;
         try {
-          const zipcode = request.params.zipcode;
-          const res = await fetch(largeJsonUrl);
-          const json = await res.json();
+          const jsonData = await readFilePromise(largeFile);
+          const json = JSON.parse(jsonData);
           const filteredResults = json.rows.filter(obj => {
             return obj.zip_code === zipcode;
           });
           response.json(filteredResults);
-        } catch (e) {
-          return next(e);
-        }
-      }
-    );
-
-    this.router.get(
-      "/data/small/net/stream/:speaker",
-      (request, response, next) => {
-        const speaker = request.params.speaker;
-        fetch(smallJsonUrl)
-          .then(res => {
-            pump(
-              res.body,
-              parseSpeakers(speaker),
-              JSONStream.stringify(),
-              response,
-              error => {
-                if (error) {
-                  return next(error);
-                }
-              }
-            );
-          })
-          .catch(e => {
-            e.status = 503;
-            return next(e);
-          });
-      }
-    );
-
-    this.router.get(
-      "/data/small/net/promise/:speaker",
-      async (request, response, next) => {
-        try {
-          const speaker = request.params.speaker;
-          const res = await fetch(smallJsonUrl);
-          const json = await res.json();
-          const filteredResults = json.results.speaker_labels.segments.filter(
-            obj => {
-              return obj.speaker_label === speaker;
-            }
-          );
-          response.json(filteredResults);
-        } catch (e) {
-          e.status = 503;
-          return next(e);
-        }
-      }
-    );
-
-    this.router.get(
-      "/data/small/disk/stream/:speaker",
-      (req, response, next) => {
-        const speaker = req.params.speaker;
-        const readStream = fs.createReadStream(
-          __dirname + "/../../data/0002-Art-Of-The-Take.json"
-        );
-        pump(
-          readStream,
-          parseSpeakers(speaker),
-          JSONStream.stringify(),
-          response,
-          error => {
-            if (error) {
-              return next(error);
-            }
+        } catch (error) {
+          if (error.code === "ENOENT") {
+            console.error("Large data file does not exist");
+            console.error("Try running `node bin/download-large-data`");
+            process.exit(-2);
           }
-        );
+        }
       }
     );
 
+    this.router.get("/data/small/stream/:speaker", (req, response, next) => {
+      const speaker = req.params.speaker;
+      const readStream = fs.createReadStream(smallData);
+      pump(
+        readStream,
+        parseSpeakers(speaker),
+        JSONStream.stringify(),
+        response,
+        error => {
+          if (error) {
+            return next(error);
+          }
+        }
+      );
+    });
+
     this.router.get(
-      "/data/small/disk/promise/:speaker",
+      "/data/small/promise/:speaker",
       async (request, response, next) => {
         try {
           const speaker = request.params.speaker;
-          const jsonData = await readFilePromise(
-            __dirname + "/../../data/0002-Art-Of-The-Take.json",
-            {
-              encoding: "utf-8"
-            }
-          );
+          const jsonData = await readFilePromise(smallData, {
+            encoding: "utf-8"
+          });
           const json = JSON.parse(jsonData);
           const filteredResults = json.results.speaker_labels.segments.filter(
             obj => {
